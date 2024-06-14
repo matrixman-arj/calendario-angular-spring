5ffdbb9465ae0a9322b36f70c5eb0323a002b075
matrixman-arj <matrixman.arj@gmail.com>
2024-06-14 07:59:30 -0300

    Iniciando o projeto, agora com spring

---
+++ calendario-spring/src/main/java/br/mil/eb/decex/calendario_spring/CalendarioSpringApplication.java
@@ -0,0 +1,13 @@
+package br.mil.eb.decex.calendario_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendarioSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalendarioSpringApplication.class, args);
+	}
+
+}
