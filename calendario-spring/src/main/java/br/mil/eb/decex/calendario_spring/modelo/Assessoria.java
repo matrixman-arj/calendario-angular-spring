355384b7e9616d6825761a7205a0c5b62bc56931
matrixman-arj <matrixman.arj@gmail.com>
2024-06-22 19:05:16 -0300

    Removendo comentarios

--- calendario-spring/src/main/java/br/mil/eb/decex/calendario_spring/modelo/Assessoria.java
+++ calendario-spring/src/main/java/br/mil/eb/decex/calendario_spring/modelo/Assessoria.java
@@ -1,10 +1,9 @@
 package br.mil.eb.decex.calendario_spring.modelo;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 
-import jakarta.persistence.CascadeType;
+
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -12,8 +11,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.NamedQuery;
-import jakarta.persistence.OneToMany;
+
 import jakarta.persistence.SequenceGenerator;
 
 @Entity
--- calendario-spring/src/main/java/br/mil/eb/decex/calendario_spring/modelo/Pessoa.java
+++ calendario-spring/src/main/java/br/mil/eb/decex/calendario_spring/modelo/Pessoa.java
@@ -18,7 +18,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.NamedQuery;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Transient;
 
