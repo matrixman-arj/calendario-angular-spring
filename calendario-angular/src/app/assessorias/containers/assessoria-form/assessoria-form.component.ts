65b9677e21075114c3793cd8f7dba3b89b795471
matrixman-arj <matrixman.arj@gmail.com>
2024-07-04 09:18:25 -0300

    Add files via upload
    Enviados por fora

---
+++ calendario-angular/src/app/assessorias/containers/assessoria-form/assessoria-form.component.ts
@@ -0,0 +1,58 @@
+
+import { Component, OnInit } from '@angular/core';
+import { UntypedFormBuilder, UntypedFormGroup } from '@angular/forms';
+import { Location } from '@angular/common';
+
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { AssessoriasService } from '../../services/assessorias.service';
+
+
+@Component({
+  selector: 'app-assessoria-form',
+  templateUrl: './assessoria-form.component.html',
+  styleUrl: './assessoria-form.component.scss'
+})
+export class AssessoriaFormComponent implements OnInit {
+
+  form: UntypedFormGroup;
+
+  constructor(private formBuilder: UntypedFormBuilder,
+    private service: AssessoriasService,
+    private snackBar: MatSnackBar,
+    private location: Location) {
+
+    this.form = this.formBuilder.group({
+
+      descricao: [null],
+      sigla: [null]
+
+    });
+  }
+
+
+
+    ngOnInit(): void {
+
+
+  }
+
+  onSubmit() {
+    this.service.save(this.form.value)
+      .subscribe(result => this.onSuccess(), error => this.onError());
+  }
+
+  onCancel() {
+    this.location.back();
+  }
+
+  private onSuccess(){
+    this.snackBar.open('Assessoria salva com sucesso!', '', { duration: 3000});
+    this.onCancel();
+}
+
+  private onError() {
+    this.snackBar.open('Erro ao salvar assessoria.', '', { duration: 3000});
+  }
+
+
+}
