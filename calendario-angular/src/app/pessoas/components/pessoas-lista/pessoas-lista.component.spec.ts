64e45dadfd0f9a4a38eba65f0f2fe3087a8fc81d
matrixman-arj <matrixman.arj@gmail.com>
2024-07-04 09:31:14 -0300

    Add files via upload

---
+++ calendario-angular/src/app/pessoas/components/pessoas-lista/pessoas-lista.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PessoasListaComponent } from './pessoas-lista.component';
+
+describe('PessoasListaComponent', () => {
+  let component: PessoasListaComponent;
+  let fixture: ComponentFixture<PessoasListaComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [PessoasListaComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(PessoasListaComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ calendario-angular/src/app/pessoas/components/pessoas-lista/pessoas-lista.component.ts
@@ -0,0 +1,34 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+import { Pessoa } from '../../model/pessoa';
+
+
+
+@Component({
+  selector: 'app-pessoas-lista',
+  templateUrl: './pessoas-lista.component.html',
+  styleUrl: './pessoas-lista.component.scss'
+})
+export class PessoasListaComponent implements OnInit {
+
+ @Input() pessoas: Pessoa[] = [];
+ @Output() add = new EventEmitter(false);
+
+
+  readonly displayedColumns = ['foto','identidade', 'nome', 'nomeGuerra', 'postoGraduacao', 'assessoria', 'ramal', 'acoes'];
+
+  // pessoasService: PessoasService;
+
+  constructor( ){  }
+
+  ngOnInit(): void {
+    // TODO document why this method 'ngOnInit' is empty
+
+  }
+
+  onAdd(){
+    this.add.emit(true);
+
+  }
+
+}
