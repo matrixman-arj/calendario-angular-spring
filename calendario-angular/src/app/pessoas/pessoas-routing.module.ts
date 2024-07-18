9a49451a2bf14a4d49e4d4220be643d68536b259
matrixman-arj <matrixman.arj@gmail.com>
2024-07-18 16:20:59 -0300

    Continuando o preenchimeto de formulário para edição de pessoa

--- calendario-angular/src/app/pessoas/pessoas-routing.module.ts
+++ calendario-angular/src/app/pessoas/pessoas-routing.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+
 import { PessoasComponent } from './containers/pessoas/pessoas.component';
-import { PessoaFormComponent } from './containers/pessoa-form/pessoa-form.component';
 import { PessoaResolver } from './guards/pessoa.resolver';
+import { PessoasFormComponent } from './containers/pessoas-form/pessoas-form.component';
 
 
 const routes: Routes = [
   { path: '', component: PessoasComponent},
-  { path: 'new', component: PessoaFormComponent},
-  { path: 'edit/:id', component: PessoaFormComponent, resolve:{pessoa: PessoaResolver}}
+  { path: 'new', component: PessoasFormComponent, resolve:{pessoa: PessoaResolver}},
+  { path: 'edit/:id', component: PessoasFormComponent, resolve:{pessoa: PessoaResolver}}
 
 ];
 
--- calendario-angular/src/app/pessoas/pessoas.module.ts
+++ calendario-angular/src/app/pessoas/pessoas.module.ts
@@ -5,7 +5,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppMaterialModule } from '../shared/app-material/app-material.module';
 import { SharedModule } from '../shared/shared.module';
 import { PessoasListaComponent } from './components/pessoas-lista/pessoas-lista.component';
-import { PessoaFormComponent } from './containers/pessoa-form/pessoa-form.component';
+import { PessoasFormComponent } from './containers/pessoas-form/pessoas-form.component';
 import { PessoasComponent } from './containers/pessoas/pessoas.component';
 import { PessoasRoutingModule } from './pessoas-routing.module';
 
@@ -15,7 +15,7 @@ import { PessoasRoutingModule } from './pessoas-routing.module';
 @NgModule({
   declarations: [
     PessoasComponent,
-    PessoaFormComponent,
+    PessoasFormComponent,
     PessoasListaComponent
   ],
   imports: [
