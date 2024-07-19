24be6306255f75d69261fb732de256aed49bc2b7
matrixman-arj <matrixman.arj@gmail.com>
2024-07-19 08:07:57 -0300

    Consertando chamada para a edição de pessoa.

--- calendario-angular/src/app/pessoas/guards/pessoa.resolver.ts
+++ calendario-angular/src/app/pessoas/guards/pessoa.resolver.ts
@@ -1,17 +1,17 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, Resolve, ResolveFn, RouterStateSnapshot } from '@angular/router';
+import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
 import { PessoasService } from '../services/pessoas.service';
 import { Pessoa } from '../model/pessoa';
 import { Observable, of } from 'rxjs';
 
-Injectable({
+@Injectable({
   providedIn: 'root'
 })
 
 export class PessoaResolver implements Resolve<Pessoa> {
   constructor(private service: PessoasService) { }
 
-  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Pessoa> {
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Pessoa> | Promise<Pessoa> | Pessoa {
     if (route.params && route.params['id']) {
       return this.service.loadById(route.params['id']);
     }
