0241ed2a16c95264793726e4f19428396ba7156e
matrixman-arj <matrixman.arj@gmail.com>
2024-07-26 12:07:51 -0300

    Add files via upload

---
+++ calendario-spring/src/main/java/br/mil/eb/decex/calendario_spring/converter/AssessoriaConverter.java
@@ -0,0 +1,22 @@
+package br.mil.eb.decex.calendario_spring.converter;
+
+import org.springframework.core.convert.converter.Converter;
+
+import br.mil.eb.decex.calendario_spring.modelo.Assessoria;
+import io.micrometer.common.util.StringUtils;
+
+public class AssessoriaConverter implements Converter<String, Assessoria>{
+
+    @Override
+	public Assessoria convert(String id) {
+		if (!StringUtils.isEmpty(id)) {
+			Assessoria asse = new Assessoria();
+			asse.setId(Long.valueOf(id));
+			return asse;
+		}
+		
+		return null;
+    
+}
+
+}
