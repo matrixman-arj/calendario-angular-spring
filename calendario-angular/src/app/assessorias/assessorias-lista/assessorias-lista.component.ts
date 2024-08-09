691e7b41b052fe5999eacf102da62b9360fa3349
matrixman-arj <matrixman.arj@gmail.com>
2024-08-09 17:19:37 -0300

    Algumas alterações de casa2

---
+++ calendario-angular/src/app/assessorias/assessorias-lista/assessorias-lista.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-assessorias-lista',
+  templateUrl: './assessorias-lista.component.html',
+  styleUrl: './assessorias-lista.component.scss'
+})
+export class AssessoriasListaComponent {
+
+}
