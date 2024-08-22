4b56310dca40b010abafd61d2e58d44efbb727cb
matrixman-arj <matrixman.arj@gmail.com>
2024-08-22 15:25:10 -0300

    Mai do DTO no back-end

--- calendario-spring/src/main/java/br/mil/eb/decex/calendario_spring/dto/AssessoriaDTO.java
+++ calendario-spring/src/main/java/br/mil/eb/decex/calendario_spring/dto/AssessoriaDTO.java
@@ -3,15 +3,24 @@ package br.mil.eb.decex.calendario_spring.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import br.mil.eb.decex.calendario_spring.modelo.Assessoria;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record AssessoriaDTO(
     
-    @JsonProperty("_id")
-    Long id, 
-    String sigla, 
-    String descricao, 
+    @JsonProperty("_id")Long id, 
+    
+    @NotBlank
+	@NotNull
+    String sigla,
+    
+    @NotBlank
+	@NotNull
+    String descricao,
+
     Assessoria assessoriaPai, 
-    int ordem, 
+    int ordem,
+     
     Boolean interna 
     ) {
 
--- calendario-spring/src/main/java/br/mil/eb/decex/calendario_spring/dto/PessoaDTO.java
+++ calendario-spring/src/main/java/br/mil/eb/decex/calendario_spring/dto/PessoaDTO.java
@@ -3,17 +3,36 @@ package br.mil.eb.decex.calendario_spring.dto;
 import br.mil.eb.decex.calendario_spring.enumerado.PostoGraduacao;
 import br.mil.eb.decex.calendario_spring.enumerado.TipoAcesso;
 import br.mil.eb.decex.calendario_spring.modelo.jaas.Users;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
-public record PessoaDTO(Long id, 
-                        String identidade, 
-                        Users users, 
-                        String nome, 
-                        String nomeGuerra, 
+public record PessoaDTO(Long id,
+                        @NotBlank
+	                    @NotNull
+                        @Pattern(regexp = "^\\d{3}\\.\\d{3}\\.\\d{3}-\\d$", message = "Formato de identidade inválido. Deve estar no formato 000.000.000-0.")                        
+                        String identidade,
+
+                        Users users,
+                        
+                        @NotBlank
+	                    @NotNull
+                        String nome,
+                        
+                        @NotBlank
+	                    @NotNull
+                        String nomeGuerra,
+
                         PostoGraduacao postoGraduacao, 
+                        
                         Boolean acesso, 
+                        
                         TipoAcesso tipoAcesso, 
+                        @Pattern(regexp = "^810 - \\d{4}$", message = "Formato do ramal inválido. Deve estar no formato 000 - 0000") 
                         String ramal, 
+                        
                         String caminho, 
+                        
                         String antiguidade  
                         ) {
 
