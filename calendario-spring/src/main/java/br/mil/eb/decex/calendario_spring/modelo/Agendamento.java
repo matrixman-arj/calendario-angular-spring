4758adb34f2229f05795ddb4e56810f7a2564b01
matrixman-arj <matrixman.arj@gmail.com>
2024-10-11 11:58:32 -0300

    Nada

--- calendario-spring/src/main/java/br/mil/eb/decex/calendario_spring/modelo/Agendamento.java
+++ calendario-spring/src/main/java/br/mil/eb/decex/calendario_spring/modelo/Agendamento.java
@@ -20,7 +20,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.SequenceGenerator;
-import jakarta.validation.constraints.NotNull;
+
 
 @Entity
 public class Agendamento {
