4c7ebcfde88a1b99b5caca8267f32c3b628d392b
matrixman-arj <matrixman.arj@gmail.com>
2024-11-20 14:31:48 -0300

    Removendo comentário desnecessários

--- calendario-angular/src/app/pessoas/components/pessoas-lista/pessoas-lista.component.ts
+++ calendario-angular/src/app/pessoas/components/pessoas-lista/pessoas-lista.component.ts
@@ -1,8 +1,8 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 import { Pessoa } from '../../model/pessoa';
-import { MatTableDataSource, MatTable, MatColumnDef, MatHeaderCellDef, MatHeaderCell, MatCellDef, MatCell, MatHeaderRowDef, MatHeaderRow, MatRowDef, MatRow } from '@angular/material/table';
-import { HttpClient } from '@angular/common/http';
+import {  MatTable, MatColumnDef, MatHeaderCellDef, MatHeaderCell, MatCellDef, MatCell, MatHeaderRowDef, MatHeaderRow, MatRowDef, MatRow } from '@angular/material/table';
+
 import { PostoGraduacao, PostoGraduacaoList } from '../../../enums/PostoGraduacao/PostoGraduacao';
 import { TipoAcesso, TipoAcessoList } from '../../../enums/TipoAcesso';
 
@@ -10,7 +10,6 @@ import { MatIcon } from '@angular/material/icon';
 import { MatIconButton } from '@angular/material/button';
 
 import { MatCard } from '@angular/material/card';
-import { PessoasService } from '../../services/pessoas.service';
 @Component({
     selector: 'app-pessoas-lista',
     templateUrl: './pessoas-lista.component.html',
@@ -33,17 +32,6 @@ export class PessoasListaComponent implements OnInit {
  @Output() delete = new EventEmitter(false);
 
 
-//  @ViewChild(MatPaginator, { static: false }) paginator!: MatPaginator;
-
-//  ngAfterViewInit() {
-//    this.dataSource.paginator = this.paginator;
-//  }
-
-//  ngOnChanges() {
-//   this.dataSource.data = this.pessoas; // Atualiza os dados do MatTableDataSource
-// }
-
-
  postos = PostoGraduacaoList;
  selectedPosto = PostoGraduacao.GEN_EXERCITO;
 
@@ -56,93 +44,15 @@ export class PessoasListaComponent implements OnInit {
 
   readonly displayedColumns = ['caminho','identidade', 'nome', 'nomeGuerra', 'postoGraduacao', 'assessoria', 'ramal', 'acoes'];
 
-  // page = 0; // Página inicial
-  // size = 10; // Itens por página
-  // termo = '';
 
-  // totalElements = 0; // Total de elementos no banco de dados
-
-
-// onPageChange(event: PageEvent): void {
-//   this.page = event.pageIndex;
-//   this.size = event.pageSize;
-//   // this.search(); // Recarrega os dados da nova página
-// }
-
-
-  constructor(
-    private http: HttpClient,
-    private pessoasService: PessoasService
-    // private shared: /* TODO(standalone-migration): clean up removed NgModule reference manually. */  SharedModule
-
-
-   ){
-
-   }
+  constructor( ){  }
 
 
 
   ngOnInit(): void {
-    // this.http.get<Pessoa[]>('/api/pessoas').subscribe(data => {
-    //   this.dataSource.data = data;
-
-
-    // });
-
-    // this.atualizarDataSource();
-
-    // this.search();
-
-    // this.dataSource.data = this.pessoas; // Inicializa os dados no DataSource
-
-
+    // Initialization logic can be added here if needed
+    console.log('PessoasListaComponent initialized');
   }
-
-  // ngOnChanges() {
-  //   // Sempre que o array de pessoas mudar, atualize o DataSource
-  //   this.atualizarDataSource();
-  // }
-
-
-  // atualizarDataSource() {
-  //   this.dataSource.data = this.pessoas;
-  //   if (this.paginator) {
-  //     this.dataSource.paginator = this.paginator; // Conectando paginação ao DataSource
-  //   }
-  // }
-
-  // aplicarFiltro(event: Event) {
-  //   const valorFiltro = (event.target as HTMLInputElement).value;
-  //   this.dataSource.filter = valorFiltro.trim().toLowerCase(); // Configurar o filtro
-
-  //   this.dataSource.filterPredicate = (data: Pessoa, filter: string) => {
-  //     const dataStr = JSON.stringify(data).toLowerCase();
-  //     return dataStr.includes(filter);
-  //   };
-  // }
-
-
-
-  // search(): void {
-  //   this.pessoasService.list(this.termo || '', this.page, this.size).subscribe(
-  //     (response) => {
-  //       this.dataSource.data = response.content; // Atualiza os dados da tabela
-  //       this.totalElements = response.totalElements; // Atualiza o total de elementos
-  //     },
-  //     (error) => {
-  //       console.error('Erro ao buscar pessoas:', error);
-  //     }
-  //   );
-  // }
-
-
-  // onSearchTermChange(event: Event): void {
-  //   const inputElement = event.target as HTMLInputElement;
-  //   const term = inputElement.value;
-  //   this.termo = term;
-  //   this.page = 0; // Reinicia a paginação
-  //   // this.search();
-  // }
 
 
   onImageError(event: Event): void {
